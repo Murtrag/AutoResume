2a491ddf4e78c2f6befd6e8468e116da523f0d74
murtrag <lukasz.czernik@gmail.com>
2020-12-01 16:21:00 +0100

    Test mysql connection

--- core/settings.py
+++ core/settings.py
@@ -64,12 +64,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', 
+            'NAME': 'AutoResume',
+            'USER': 'Murtrag',
+            'PASSWORD': 'kajtek123',
+            'HOST': 'Murtrag.mysql.pythonanywhere-services.com',   # Or an IP Address that your DB is hosted on
+            'PORT': '3306',
+            }
+        }
+
 
 
 # Password validation
