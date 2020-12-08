e22b3050d2ae45921721503e57e7aff9310429f9
murtrag <lukasz.czernik@gmail.com>
2020-12-08 02:25:23 +0100

    Added fields to admin model display and some basic filters closes #1

--- resume/admin.py
+++ resume/admin.py
@@ -1,9 +1,46 @@
 from django.contrib import admin
 from resume import models
 
-admin.site.register(models.BasicInfo)
-admin.site.register(models.Section)
-admin.site.register(models.ListItem)
-admin.site.register(models.GraphItem)
-admin.site.register(models.SectionContent)
-admin.site.register(models.GitHubButton)
+
+class BasicInfoAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "info_id")
+
+
+admin.site.register(models.BasicInfo, BasicInfoAdmin)
+
+
+class GitHubButtonAdmin(admin.ModelAdmin):
+    list_display = ("user", "url")
+
+
+admin.site.register(models.GitHubButton, GitHubButtonAdmin)
+
+
+class SectionAdmin(admin.ModelAdmin):
+    list_filter = ("user",)
+    list_display = ("user", "name", "section_type", "position")
+
+
+admin.site.register(models.Section, SectionAdmin)
+
+
+class ListItemAdmin(admin.ModelAdmin):
+    list_display = ("headline", "year")
+
+
+admin.site.register(models.ListItem, ListItemAdmin)
+
+
+class GraphItemAdmin(admin.ModelAdmin):
+    list_filter = ("category",)
+    list_display = ("category", "name", "level")
+
+
+admin.site.register(models.GraphItem, GraphItemAdmin)
+
+
+class SectionContentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+admin.site.register(models.SectionContent, SectionContentAdmin)
