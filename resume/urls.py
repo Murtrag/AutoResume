281c32f28b484767c60132929dcc551de8ffab82
murtrag <lukasz.czernik@gmail.com>
2021-02-15 15:05:59 +0100

    Added print View

--- resume/urls.py
+++ resume/urls.py
@@ -3,4 +3,11 @@ from django.urls import path, re_path
 from . import views
 
 
-urlpatterns = [re_path("(?P<info_id>\w+)$", views.display_resume, name="resume")]
+urlpatterns = [
+    re_path(
+        "^(?P<info_id>\w+)/print/$",
+        views.DisplayPrintResume.as_view(),
+        name="print_resume",
+    ),
+    re_path("(?P<info_id>\w+)", views.DisplayResume.as_view(), name="resume"),
+]
--- resume/views.py
+++ resume/views.py
@@ -3,14 +3,15 @@ from django.http import HttpResponse
 from . import models
 from briefcase.models import Type
 from django.db.models import Prefetch
+from django.views import View
 
 
-def display_resume(request, info_id):
-    basic_info = models.BasicInfo.objects.get(info_id=info_id)
-    return render(
-        request,
-        "resume/resume.html",
-        {
+class DisplayResume(View):
+    template = "resume/resume.html"
+
+    def _get_context(self, request, info_id):
+        basic_info = models.BasicInfo.objects.get(info_id=info_id)
+        return {
             "basic_info": basic_info,
             "sections": models.Section.objects.filter(user=basic_info)
             .order_by("position")
@@ -23,5 +24,11 @@ def display_resume(request, info_id):
                 )
             ),
             "section_types": {key: value for value, key in models.section_types},
-        },
-    )
+        }
+
+    def get(self, request, info_id):
+        return render(request, self.template, self._get_context(request, info_id))
+
+
+class DisplayPrintResume(DisplayResume):
+    template = "resume/print.html"
