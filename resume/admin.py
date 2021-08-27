3afc6deab24db2bd3b5288e017e4cb58825928b3
murtrag <lukasz.czernik@gmail.com>
2021-08-27 18:50:48 +0200

    Added section field to ListItemAdmin

--- resume/admin.py
+++ resume/admin.py
@@ -31,9 +31,6 @@ class SectionAdmin(admin.ModelAdmin):
     list_display = ("user", "name", "section_type", "position")
 
 
-@admin.register(models.ListItem)
-class ListItemAdmin(admin.ModelAdmin):
-    list_display = ("headline", "year", "position")
 
 
 @admin.register(models.GraphItem)
@@ -49,7 +46,7 @@ class LanguageAdmin(admin.ModelAdmin):
 
 class SectionContentUserFilter(admin.SimpleListFilter):
     title = ('user')
-    parameter_name = 'userek'
+    parameter_name = 'user'
 
     def lookups(self, request, model_admin):
         list = models.BasicInfo.objects.all().values_list('name', 'language', 'pk')
@@ -77,3 +74,35 @@ class SectionContentAdmin(admin.ModelAdmin):
     list_filter = (SectionContentUserFilter,)
     list_display = ("name", "section")
 
+
+
+class ListItemUserFilter(admin.SimpleListFilter):
+    title = ('user')
+    parameter_name = 'user'
+
+    def lookups(self, request, model_admin):
+        list = models.BasicInfo.objects.all().values_list('name', 'language', 'pk')
+        LanguageDict = {key: val for key, val in models.Language.objects.all().values_list("pk", "name")}
+        formated = [(x[2], (f'{x[0]} - {LanguageDict[x[1]]}')) for x in list]
+        return formated
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return None
+        raw_queryset = models.SectionContent.objects.filter(section__user__pk=self.value())
+        queryset = raw_queryset.first().list_item.none()
+        for section in raw_queryset:
+            queryset |= section.list_item.all()
+        if len(queryset) == 0:
+            return models.ListItem.objects.none()
+        return queryset 
+
+
+@admin.register(models.ListItem)
+class ListItemAdmin(admin.ModelAdmin):
+    def section(self, obj):
+        section = models.Section.objects.filter(content__list_item__in=[obj]).first()
+        return section
+
+    list_filter = (ListItemUserFilter, )
+    list_display = ("headline", "year", "position", "section")
