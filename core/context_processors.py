71f62c81e97348fffd78be0338385044895d2f23
murtrag <lukasz.czernik@gmail.com>
2021-10-05 21:42:01 +0200

    Updated context manager

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,10 +1,14 @@
 from resume.models import UrlButton
 from briefcase.models import Type
+from resume.models import BasicInfo
 
 
 def global_vars(request):
     context = dict()
-    info_id = request.resolver_match.kwargs.get("info_id")
+    if request.path == '/':
+        info_id = BasicInfo.objects.get(is_homepage=True).info_id
+    else:
+        info_id = request.resolver_match.kwargs.get("info_id")
     url_buttons = UrlButton.objects.filter(user__info_id=info_id)
     if url_buttons is not None:
         context.update(
