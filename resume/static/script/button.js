17631833b8a699cee39558d184bfc2aaac6488b7
murtrag <lukasz.czernik@gmail.com>
2021-10-06 05:57:31 +0200

    Adjusted template

--- resume/static/script/button.js
+++ resume/static/script/button.js
@@ -1,4 +1,8 @@
 function toggleClassHover(el){
-$(el.target.parentElement.firstElementChild).toggleClass('svg-button-hover');
+$(el.target.parentElement.firstElementChild).toggleClass('svg-button-hover-bg');
+let thisElParent = $(this).parent()
+thisElParent.toggleClass('svg-button-hover-bold')
+// thisElParent.first().toggleClass('svg-button-hover-bg')
+// thisElParent.filter('path').toggleClass('svg-button-hover-bg')
 }
 $(".svg-button a").mouseover(toggleClassHover ).mouseout(toggleClassHover)
