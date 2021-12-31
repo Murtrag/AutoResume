a93c3acb840cb98e4351cda6a9d46df24fb62c31
murtrag <lukasz.czernik@gmail.com>
2021-12-31 11:30:18 +0100

    Add image and desc support in modal Fix #32

--- resume/static/script/button.js
+++ resume/static/script/button.js
@@ -2,7 +2,5 @@ function toggleClassHover(el){
 $(el.target.parentElement.firstElementChild).toggleClass('svg-button-hover-bg');
 let thisElParent = $(this).parent()
 thisElParent.toggleClass('svg-button-hover-bold')
-// thisElParent.first().toggleClass('svg-button-hover-bg')
-// thisElParent.filter('path').toggleClass('svg-button-hover-bg')
 }
 $(".svg-button a").mouseover(toggleClassHover ).mouseout(toggleClassHover)
