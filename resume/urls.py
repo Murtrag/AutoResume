1862f844a5a391636c7516f072397a0c5b61ce3c
murtrag <lukasz.czernik@gmail.com>
2022-05-16 18:54:54 +0200

    print qf

--- resume/urls.py
+++ resume/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index_view, name="homepage"),
     re_path("resume/hr/(?P<info_id>\w+)", views.DisplayHackerResume.as_view(), name="hacker_resume"),
     path('i18n/', include('django.conf.urls.i18n')),
-    re_path("resume/st/(?P<info_id>\w+)", views.DisplayResume.as_view(), name="resume"),
+    re_path("resume/st/(?P<info_id>\w+)/$", views.DisplayResume.as_view(), name="resume"),
     re_path(
         "resume/(?P<info_id>\w+)/print/$",
         views.DisplayPrintResume.as_view(),
